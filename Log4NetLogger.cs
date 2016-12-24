2a54ca46f62f55d4d5b957a23e2999126fbda01a
IfteAmjad-Intelligenz <Ifte.Amjad@intelligenzsolutions.com>
2016-12-24 16:48:53 +0000

    Log4NetLogger enhanced

--- Log4NetLogger.cs
+++ Log4NetLogger.cs
@@ -9,7 +9,13 @@ namespace PlayGround
 {
     class Log4NetLogger : ILogger
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Log4NetLogger));
+        private static readonly bool isErrorEnabled = log.IsErrorEnabled;
+        private static readonly bool isWarnEnabled = log.IsWarnEnabled;
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+        private static readonly bool isInfoEnabled = log.IsInfoEnabled;
+        private static readonly bool isFatalEnabled = log.IsFatalEnabled;
+
 
         // 2016-03-03 09:00:59,076 DEBUG thread=1 [{log4net:HostName=Deer}] Intelligenz.Utils.AssemblyInitialise.Initialise(c:\TFS\Dev\Source\IntelliFitness\Utils\AssemblyInitialise.cs:44) [logging system configured]
 
@@ -24,62 +30,77 @@ namespace PlayGround
 
         public void Error(string message, Exception exception = null)
         {
-            string Message = message;
-
-            if (exception != null)
+            if (isErrorEnabled)
             {
-                Message = Message + " [System Exception: " + exception.Message + "]";
-            }
+                string Message = message;
 
-            log.Error(Message);
+                if (exception != null)
+                {
+                    Message = Message + " [System Exception: " + exception.Message + "]";
+                }
+
+                log.Error(Message);
+            }
         }
 
         public void Warn(string message, Exception exception = null)
         {
-            string Message = message;
-
-            if (exception != null)
+            if (isWarnEnabled)
             {
-                Message = Message + " [System Exception: " + exception.Message + "]";
-            }
+                string Message = message;
 
-            log.Warn(Message);
+                if (exception != null)
+                {
+                    Message = Message + " [System Exception: " + exception.Message + "]";
+                }
+
+                log.Warn(Message);
+            }
         }
 
         public void Debug(string message, Exception exception = null)
         {
-            string Message = message;
-
-            if (exception != null)
+            if (isDebugEnabled)
             {
-                Message = Message + " [System Exception: " + exception.Message + "]";
-            }
+                string Message = message;
 
-            log.Debug(Message);
+                if (exception != null)
+                {
+                    Message = Message + " [System Exception: " + exception.Message + "]";
+                }
+
+                log.Debug(Message);
+            }
         }
 
         public void Info(string message, Exception exception = null)
         {
-            string Message = message;
-
-            if (exception != null)
+            if (isInfoEnabled)
             {
-                Message = Message + " [System Exception: " + exception.Message + "]";
-            }
+                string Message = message;
 
-            log.Info(Message);
+                if (exception != null)
+                {
+                    Message = Message + " [System Exception: " + exception.Message + "]";
+                }
+
+                log.Info(Message);
+            }
         }
 
         public void Fatal(string message, Exception exception = null)
         {
-            string Message = message;
-
-            if (exception != null)
+            if (isFatalEnabled)
             {
-                Message = Message + " [System Exception: " + exception.Message + "]";
-            }
+                string Message = message;
 
-            log.Fatal(Message);
+                if (exception != null)
+                {
+                    Message = Message + " [System Exception: " + exception.Message + "]";
+                }
+
+                log.Fatal(Message);
+            }
         }
     }
 }
